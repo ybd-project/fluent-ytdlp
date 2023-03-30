86a8f7ddb3358bfbb0950d8a53b5987917764485
ybd-project <95850410+gitmcmr@users.noreply.github.com>
2023-03-30 15:15:08 +0900

    2023/03/30 15:30

---
+++ README.md
@@ -0,0 +1,3 @@
+# fluent-ytdlpに関するお知らせ
+
+このパッケージは諸事情により削除され、新たに[`node-fluent-ytdlp`](https://www.npmjs.com/package/node-fluent-ytdlp?activeTab=readme)として公開されています。
---
+++ index.js
@@ -0,0 +1,4 @@
+'use strict';
+
+console.log('このパッケージは現在、「node-fluent-ytdlp」として新たに公開されています。');
+console.log('詳細: https://www.npmjs.com/package/node-fluent-ytdlp');
